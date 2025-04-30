a7e0daab61d0cb56c4d4193244479b45b2f35e93
lwsan12 <lwsan12@winbit.kr>
2025-04-30 16:22:41 +0900

    관리자 CRUD 기초 작성

--- between/src/main/java/com/wb/between/admin/coupon/dto/AdminCouponResDto.java
+++ between/src/main/java/com/wb/between/admin/coupon/dto/AdminCouponResDto.java
@@ -4,6 +4,7 @@ import com.wb.between.coupon.domain.Coupon;
 import jakarta.persistence.Entity;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -11,13 +12,16 @@ import java.time.LocalDateTime;
 @Builder
 public class AdminCouponResDto {
 
-    // --- Coupon 정보 ---
     private Long cpNo;          // 쿠폰 번호 (Coupon 엔티티의 ID)
 
     private String cpnNm;         // 쿠폰 이름 (Coupon 엔티티 필드)
 
     private String discountInfo;  // 할인 정보 (가공된 문자열, 예: "10%", "5000원")
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime cpnStartDt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime cpnEndDt;   // 쿠폰 만료일 (Coupon 엔티티 필드) - 타입 주의
 
     private String cpnDsc;        // 쿠폰 설명 (Coupon 엔티티 필드)
@@ -25,8 +29,6 @@ public class AdminCouponResDto {
     private String discount;
 
     private String discountAt;
-
-    private LocalDateTime cpnStartDt;
 
     private String activeYn;
 
@@ -41,6 +43,7 @@ public class AdminCouponResDto {
                 .cpnStartDt(coupon.getCpnStartDt())
                 .cpnEndDt(coupon.getCpnEndDt())
                 .cpnDsc(coupon.getCpnDsc())
+                .activeYn(coupon.getActiveYn())
                 .build();
     }
 }
--- between/src/main/java/com/wb/between/coupon/domain/Coupon.java
+++ between/src/main/java/com/wb/between/coupon/domain/Coupon.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Builder
 @Entity
 @Getter
 @Setter
