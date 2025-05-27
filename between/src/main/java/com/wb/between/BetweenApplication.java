bf5891ea97750dd9d4c0c4d7faa9d57ef97801b9
lwsan12 <lwsan12@winbit.kr>
2025-05-27 17:02:59 +0900

    feat: war 배포용 수정

--- between/src/main/java/com/wb/between/BetweenApplication.java
+++ between/src/main/java/com/wb/between/BetweenApplication.java
@@ -3,16 +3,23 @@ package com.wb.between;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.wb.between", "com.another.package"}) // Controller가 있는 패키지 포함
-public class BetweenApplication {
+public class BetweenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetweenApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BetweenApplication.class);
 	}
 
 }
